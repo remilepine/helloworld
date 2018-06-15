e2a6d459b6952dfa0356645d122531e7fdcf8c12
Rémi <remi@mgig.fr>
2018-06-15 14:43:23 +0200

    print hello world

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,25 @@
-print("hello world !!")
+# singes = ['pierre', 'marie']
+#
+# for singe in singes:
+#     if singe == 'pierre':
+#         print('banane jaune')
+#     else:
+#         print('banane verte')
+
+
+class singe:
+    def __init__(self,prenom):
+        self.prenom = prenom
+
+    def returnBanane(self):
+        if self.prenom == 'pierre':
+            banane = 'banane jaune'
+        else:
+            banane = 'banane verte'
+        return banane
+
+singes = ['pierre', 'marie']
+
+for unSinge in singes:
+    leSinge = singe(unSinge)
+    print(leSinge.returnBanane())
